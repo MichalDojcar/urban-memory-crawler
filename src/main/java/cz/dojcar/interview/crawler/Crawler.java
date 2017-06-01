ed34ce78931e04339201172d2e94d06bf28d8f02
Michal Dojcar <md.mamut@seznam.cz>
2017-06-01 15:11:53 +0200

    Initiate the project with basic test

---
+++ src/main/java/cz/dojcar/interview/crawler/Crawler.java
@@ -0,0 +1,7 @@
+package cz.dojcar.interview.crawler;
+
+/**
+ * Created by michaldojcar on 01.06.2017
+ */
+public class Crawler {
+}
---
+++ src/test/java/cz/dojcar/interview/crawler/CrawlerTest.java
@@ -0,0 +1,14 @@
+package cz.dojcar.interview.crawler;
+
+import org.junit.Test;
+
+/**
+ * Created by michaldojcar on 01.06.2017
+ */
+public class CrawlerTest {
+
+    @Test
+    public void crawler_can_be_created() throws Exception {
+        Crawler crawler = new Crawler();
+    }
+}
